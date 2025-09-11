42315de6b7f404a4ddc31da3ef568c5309e5f112
ASSUS <ASSUS@DESKTOP-HKMOJ08>
2025-09-10 23:09:50 -0500

    avance codigo

--- MPLABXProjects/ledon.X/main.s
+++ MPLABXProjects/ledon.X/main.s
@@ -1,131 +1,127 @@
-;=========================================================
-; PIC18F4550 | XC8 pic-as (.s)
-; LED RB0, RB1, RB2, RB3: ON 5 s, OFF 2 s  | INTOSC = 8 MHz
-;=========================================================
+    LIST    P=18F4550
+    #include <xc.inc>
 
-; ---- FUSES ----
-    CONFIG  FOSC   = INTOSCIO_EC
-    CONFIG  FCMEN  = OFF
-    CONFIG  IESO   = OFF
-    CONFIG  WDT    = OFF
+    CONFIG  FOSC = INTOSCIO_EC
+    CONFIG  FCMEN = OFF
+    CONFIG  IESO = OFF
+    CONFIG  WDT = OFF
     CONFIG  PBADEN = OFF
-    CONFIG  LVP    = OFF
+    CONFIG  LVP = OFF
 
-#include <xc.inc>
-
-;=========================================================
-; Vector de RESET
-;=========================================================
-    PSECT   resetVec, class=CODE, abs, delta=2
-    ORG     0x0000
-    GOTO    Inicio
-
-;=========================================================
-; Variables
-;=========================================================
     PSECT   udata_acs
-ContadorExterno:   DS 1
-ContadorInterno:   DS 1
+CONTADOR:   DS 1
+TIEMPO1:    DS 1
+TIEMPO2:    DS 1
+TIEMPO3:    DS 1
+PAUSA:      DS 1
+TEMP:       DS 1
+BOTON_ANT:  DS 1
 
-;=========================================================
-; Programa principal
-;=========================================================
-    PSECT   main_code, class=CODE, reloc=2
+    PSECT   resetVec,class=CODE,reloc=2
+    GOTO    INICIO
 
-Inicio:
-    ; ----- Configuración reloj: 8 MHz -----
-    MOVLW   0x72
-    MOVWF   OSCCON
-    CLRF    OSCTUNE
-
-    ; ----- Digital I/O -----
+    PSECT   code
+INICIO:
     MOVLW   0x0F
-    MOVWF   ADCON1
+    MOVWF   ADCON1, A
     MOVLW   0x07
-    MOVWF   CMCON
+    MOVWF   CMCON, A
+    MOVLW   0x72
+    MOVWF   OSCCON, A
 
-    ; ----- PORTB salida, LEDs OFF -----
-    CLRF    TRISB
-    BCF     LATB,0
-    BCF     LATB,1
-    BCF     LATB,2
-    BCF     LATB,3
+    MOVLW   0xF0           ; RB7..RB4=1 (entradas), RB3..RB0=0 (salidas)
+    MOVWF   TRISB, A
+    BCF     INTCON2,7      ; habilita pull-ups en PORTB
+    CLRF    LATB, A
 
-Loop:
-    ; ---------- LEDS ON 5 s ----------
-    BSF     LATB,0      ; LED1 (RB0) ON
-    BSF     LATB,1      ; LED2 (RB1) ON
-    BSF     LATB,2      ; LED3 (RB2) ON
-    BSF     LATB,3      ; LED4 (RB3) ON
-    CALL    Retardo_1s
-    CALL    Retardo_1s
-    CALL    Retardo_1s
-    CALL    Retardo_1s
-    CALL    Retardo_1s
+    CLRF    PAUSA, A
+    MOVLW   1              ; botón suelto (RB4=1 por pull-up)
+    MOVWF   BOTON_ANT, A
 
-    ; ---------- LEDS OFF 2 s ----------
-    BCF     LATB,0      ; LED1 (RB0) OFF
-    BCF     LATB,1      ; LED2 (RB1) OFF
-    BCF     LATB,2      ; LED3 (RB2) OFF
-    BCF     LATB,3      ; LED4 (RB3) OFF
-    CALL    Retardo_1s
-    CALL    Retardo_1s
+MAIN_LOOP:
+    BSF     LATB,0, A
+    CALL    RETARDO_1S
+    CALL    ESPERA_PAUSA
 
-    GOTO    Loop
+    BCF     LATB,0, A
+    CALL    RETARDO_1S
+    CALL    ESPERA_PAUSA
 
-;=========================================================
-; Retardos
-;=========================================================
-Retardo_1s:
-    MOVLW   250
-    MOVWF   ContadorExterno
-R1s_B0:
-    CALL    Delay_1ms
-    DECFSZ  ContadorExterno, F
-    GOTO    R1s_B0
+    BRA     MAIN_LOOP
 
-    MOVLW   250
-    MOVWF   ContadorExterno
-R1s_B1:
-    CALL    Delay_1ms
-    DECFSZ  ContadorExterno, F
-    GOTO    R1s_B1
-
-    MOVLW   250
-    MOVWF   ContadorExterno
-R1s_B2:
-    CALL    Delay_1ms
-    DECFSZ  ContadorExterno, F
-    GOTO    R1s_B2
-
-    MOVLW   250
-    MOVWF   ContadorExterno
-R1s_B3:
-    CALL    Delay_1ms
-    DECFSZ  ContadorExterno, F
-    GOTO    R1s_B3
-
+RETARDO_1S:
+    MOVLW   0x20
+    MOVWF   TIEMPO3, A
+R3:
+    MOVLW   0xFF
+    MOVWF   TIEMPO2, A
+R2:
+    MOVLW   0xFF
+    MOVWF   TIEMPO1, A
+R1:
+    DECFSZ  TIEMPO1, F, A
+    BRA     R1
+    DECFSZ  TIEMPO2, F, A
+    BRA     R2
+    DECFSZ  TIEMPO3, F, A
+    BRA     R3
     RETURN
 
-Delay_1ms:
-    MOVLW   200
-    MOVWF   ContadorInterno
-D1ms_L1:
+ESPERA_PAUSA:
+    CALL    CHECK_BOTON_TOGGLE
+    MOVF    PAUSA, W, A
+    BZ      EP_EXIT
+EP_LOOP:
+    CALL    CHECK_BOTON_TOGGLE
     NOP
     NOP
-    DECFSZ  ContadorInterno, F
-    GOTO    D1ms_L1
+    MOVF    PAUSA, W, A
+    BNZ     EP_LOOP
+EP_EXIT:
+    RETURN
 
-    MOVLW   200
-    MOVWF   ContadorInterno
-D1ms_L2:
-    NOP
-    NOP
-    DECFSZ  ContadorInterno, F
-    GOTO    D1ms_L2
+CHECK_BOTON_TOGGLE:
+    BTFSC   PORTB,4, A
+    GOTO    BTN_SUELTO1
+    CLRF    TEMP, A
+    GOTO    BTN_SAMPLE1
+BTN_SUELTO1:
+    MOVLW   1
+    MOVWF   TEMP, A
+BTN_SAMPLE1:
+    MOVF    TEMP, W, A
+    XORWF   BOTON_ANT, W, A
+    BZ      CBT_END
 
+    CALL    DEBOUNCE_20MS
+
+    BTFSC   PORTB,4, A
+    GOTO    BTN_SUELTO2
+    CLRF    TEMP, A
+    GOTO    BTN_SAMPLE2
+BTN_SUELTO2:
+    MOVLW   1
+    MOVWF   TEMP, A
+BTN_SAMPLE2:
+    MOVF    TEMP, W, A
+    MOVWF   BOTON_ANT, A
+    MOVF    TEMP, W, A
+    BNZ     CBT_END
+    MOVLW   1
+    XORWF   PAUSA, F, A
+CBT_END:
+    RETURN
+
+DEBOUNCE_20MS:
+    MOVLW   100
+    MOVWF   CONTADOR, A
+DB_L:
     NOP
     NOP
+    NOP
+    NOP
+    DECFSZ  CONTADOR, F, A
+    BRA     DB_L
     RETURN
 
     END
