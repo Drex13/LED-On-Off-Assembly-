ac95cd337ed3ecd8e24fdde8c6f229e4217f6e86
Drex <juan18834@gmail.com>
2025-09-15 03:00:52 -0500

    Agregar Boton

--- cod1.asm
+++ cod1.asm
@@ -1,11 +1,10 @@
-; Proyecto: Secuencia de 4 LEDs con 2 modos
+; Proyecto: Secuencia de 4 LEDs con 2 modos y botón
 ; PIC18F4550
 ; LEDs en RC0, RC1, RC2, RC3
-; Secuencia 1: uno por uno
-; Secuencia 2: ida y vuelta (chenille)
+; Botón en RB0 (cambia entre Secuencia 1 y 2)
 
         LIST    P=18F4550
-        INCLUDE <P18F4550.INC>
+        #include <xc.inc>
         CONFIG  FOSC=INTOSCIO_EC, WDT=OFF, LVP=OFF, PBADEN=OFF
 
         PSECT  resetVec, class=CODE, reloc=2
@@ -25,15 +24,52 @@ PSECT  main_code, class=CODE, reloc=2
     CLRF    LATC          ; limpiar puerto C
     CLRF    TRISC         ; puerto C como salida
 
+    BSF     TRISB,0       ; RB0 como entrada para botón
+
+    CLRF    Modo          ; Modo = 0 (Secuencia1 por defecto)
+    CLRF    BotonAnt      ; Estado anterior del botón
+
+; ---------- Bucle Principal ----------
 CICLO:
-    ; -------- SECUENCIA 1 --------
+    ; Leer botón en RB0
+    BTFSC   PORTB,0       ; si RB0 = 1
+    GOTO    BotonPres
+    CLRF    BotonNow
+    GOTO    BotonCheck
+BotonPres:
+    MOVLW   1
+    MOVWF   BotonNow
+
+BotonCheck:
+    ; Detectar cambio de estado
+    MOVF    BotonNow,W
+    SUBWF   BotonAnt,W
+    BZ      NoCambio       ; si no cambió, continuar
+
+    ; Hubo cambio, actualizar BotonAnt
+    MOVF    BotonNow,W
+    MOVWF   BotonAnt
+
+    ; Si es flanco de subida (BotonNow = 1), cambiar modo
+    MOVF    BotonNow,W
+    BZ      NoCambio        ; si es 0, no hacer nada
+    INCF    Modo,F
+    MOVLW   2
+    CPFSGT  Modo
+    CLRF    Modo            ; reinicia a 0 si Modo > 1
+
+NoCambio:
+    ; Ejecutar secuencia según Modo
+    MOVF    Modo,W
+    BZ      RunSeq1
+    GOTO    RunSeq2
+
+RunSeq1:
     CALL    SECUENCIA1
-    CALL    DELAY2S        ; pausa entre secuencias
+    GOTO    CICLO
 
-    ; -------- SECUENCIA 2 --------
+RunSeq2:
     CALL    SECUENCIA2
-    CALL    DELAY2S        ; pausa entre secuencias
-
     GOTO    CICLO
 
 ; -----------------------------------------
@@ -59,7 +95,7 @@ SECUENCIA1:    ; Encender LEDs uno por uno
 
     RETURN
 
-SECUENCIA2:    ; 
+SECUENCIA2:    ; Ida y vuelta
     ; Ida
     BSF     LATC,0
     CALL    DELAY1S
@@ -104,13 +140,5 @@ L2:     DECFSZ  0x21,F
         GOTO    L1
         RETURN
 
-; Retardo de 2 segundos (2 veces DELAY1S)
-DELAY2S:
-        MOVLW   D'2'
-        MOVWF   0x22
-L3:     CALL    DELAY1S
-        DECFSZ  0x22,F
-        GOTO    L3
-        RETURN
 
         END
